35b776658ffea93916b6cb5e9c80d351a77c0df1
nasuda <nasuda908@gmail.com>
2024-02-29 22:12:31 +0900

    replace html add new html

--- read.mjs
+++ read.mjs
@@ -1,4 +1,6 @@
 import { readFileSync } from 'node:fs'
+import { writeFile } from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
 
 const srcFile = readFileSync('./src.html', 'utf8')
 console.log(srcFile)
@@ -7,3 +9,12 @@ console.log(srcFile)
 const html = srcFile.replace('<!--replace-->', 'repalace!')
 
 console.log(html)
+
+writeFile('./dist.html', html)
+
+async function check() {
+  const distHtml =  await readFile('./dist.html', 'utf8')
+  console.log(distHtml)
+}
+
+check()
